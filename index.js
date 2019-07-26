4c4b3d1eb963a8ab0c2569cd3cf729e94d5d1ea1
Jimoh Lukman Adeyemi <lafbnyunus@yahoo.com>
2019-07-26 17:14:42 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-<script type="text/javascript">
-  var image=document.getElementById("bgpix")
-var imageArray=["images.jpg","banner.jpg","whitestudents.jpg","mer.jpg"];
+var image=document.getElementById("slideimages")
+var imageArray=["images/piano.jpg","images/computer.jpg.jpg"];
 var imageIndex=0;
 function changeImage(){
 image.setAttribute("src",imageArray[imageIndex]);
@@ -9,4 +8,4 @@ if(imageIndex >= imageArray.length){
 imageIndex=0;
 }
 }
-setInterval(changeImage,3000);
+setInterval(changeImage,5000);
